5da14016b5e545295d0e54f559fa2522da5cf24f
willyam_evangelista <willyam_evangelista@SNSOO1EP01VM135.saojose.sc.senai.br>
2015-10-26 22:01:42 -0200

    Criação da entidade Usuarios - Willyam Evangelista / João Souza

---
+++ RHFacil/src/entity/EntidadeUsuarios.java
@@ -0,0 +1,55 @@
+
+package entity;
+
+/**
+ *
+ * @author willyam evangelista / João Souza
+ */
+public class EntidadeUsuarios {
+    private String nomeUsuarios;
+    private String senhaUsuarios;
+    private String cpfUsuarios;
+    private String emailUsuarios;
+    private String perfilUsuarios;
+
+    public String getNomeUsuarios() {
+        return nomeUsuarios;
+    }
+
+    public void setNomeUsuarios(String nomeUsuarios) {
+        this.nomeUsuarios = nomeUsuarios;
+    }
+
+    public String getSenhaUsuarios() {
+        return senhaUsuarios;
+    }
+
+    public void setSenhaUsuarios(String senhaUsuarios) {
+        this.senhaUsuarios = senhaUsuarios;
+    }
+
+    public String getCpfUsuarios() {
+        return cpfUsuarios;
+    }
+
+    public void setCpfUsuarios(String cpfUsuarios) {
+        this.cpfUsuarios = cpfUsuarios;
+    }
+
+    public String getEmailUsuarios() {
+        return emailUsuarios;
+    }
+
+    public void setEmailUsuarios(String emailUsuarios) {
+        this.emailUsuarios = emailUsuarios;
+    }
+
+    public String getPerfilUsuarios() {
+        return perfilUsuarios;
+    }
+
+    public void setPerfilUsuarios(String perfilUsuarios) {
+        this.perfilUsuarios = perfilUsuarios;
+    }
+   
+}
