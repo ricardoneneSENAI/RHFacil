4164424e6d45b5a093815f2fdc391276a69826bc
ricardo.nene <ricardo.nene@SNSOO1EP01VM138.saojose.sc.senai.br>
2015-10-30 19:02:26 -0200

    Classe de Conexão

---
+++ RHFacil/src/dao/ConnectionManager.java
@@ -0,0 +1,29 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class ConnectionManager {
+
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+
+        if (conn == null) {
+            String STR_DRIVER = "org.gjt.mm.mysql.Driver";
+            String STR_CONEX = "jdbc:mysql://localhost:3306/DBRHFACIL";
+            String USER = "root";
+            String PASSWORD = "";
+            
+            try {
+                Class.forName(STR_DRIVER);
+                conn = DriverManager.getConnection(STR_CONEX, USER, PASSWORD);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Erro de conexão com o banco de dados!");
+            }
+        }
+        return conn;
+    }
+}
