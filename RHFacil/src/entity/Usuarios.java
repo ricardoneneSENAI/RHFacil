4b3754b89a477e032bf24f6a3cb94a8493fcfd21
joao_souza_lopes <joao_souza_lopes@SNSOO1EP01VM157.saojose.sc.senai.br>
2015-11-06 21:07:07 -0200

    Alteracao da classe entidade

--- RHFacil/src/entity/Usuarios.java
+++ RHFacil/src/entity/Usuarios.java
@@ -6,74 +6,52 @@ package entity;
  * @author willyam evangelista / João Souza
  */
 public class Usuarios {
-    private Integer IdUsuarios;
-    private String nomeUsuarios;
-    private String senhaUsuarios;
-    private String cpfUsuarios;
-    private String emailUsuarios;
-    private String perfilUsuarios;
-    
-public Usuarios (){
-
-}
-    public Usuarios(Integer IdUsuarios, String nomeUsuarios, String senhaUsuarios, String cpfUsuarios, String emailUsuarios, String perfilUsuarios) {
-        this.IdUsuarios = IdUsuarios;
-        this.nomeUsuarios = nomeUsuarios;
-        this.senhaUsuarios = senhaUsuarios;
-        this.cpfUsuarios = cpfUsuarios;
-        this.emailUsuarios = emailUsuarios;
-        this.perfilUsuarios = perfilUsuarios;
-    }
-    
-   
-    
+    private Integer idUsuarios;
+    private String nome;
+    private String senha;
+    private String email;
+    private String perfil;
 
     public Integer getIdUsuarios() {
-        return IdUsuarios;
+        return idUsuarios;
     }
 
-    public void setIdUsuarios(Integer IdUsuarios) {
-        this.IdUsuarios = IdUsuarios;
-    }
-    
-    public String getNomeUsuarios() {
-        return nomeUsuarios;
+    public void setIdUsuarios(Integer idUsuarios) {
+        this.idUsuarios = idUsuarios;
     }
 
-    public void setNomeUsuarios(String nomeUsuarios) {
-        this.nomeUsuarios = nomeUsuarios;
+    public String getNome() {
+        return nome;
     }
 
-    public String getSenhaUsuarios() {
-        return senhaUsuarios;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
-    public void setSenhaUsuarios(String senhaUsuarios) {
-        this.senhaUsuarios = senhaUsuarios;
+    public String getSenha() {
+        return senha;
     }
 
-    public String getCpfUsuarios() {
-        return cpfUsuarios;
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 
-    public void setCpfUsuarios(String cpfUsuarios) {
-        this.cpfUsuarios = cpfUsuarios;
+    public String getEmail() {
+        return email;
     }
 
-    public String getEmailUsuarios() {
-        return emailUsuarios;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public void setEmailUsuarios(String emailUsuarios) {
-        this.emailUsuarios = emailUsuarios;
+    public String getPerfil() {
+        return perfil;
     }
 
-    public String getPerfilUsuarios() {
-        return perfilUsuarios;
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
     }
-
-    public void setPerfilUsuarios(String perfilUsuarios) {
-        this.perfilUsuarios = perfilUsuarios;
-    }
-   
 }
+  
+    
+
