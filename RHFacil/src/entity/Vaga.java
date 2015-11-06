f8723443d61ab7427203915ff37e53241da044c5
lucas_medeiros1 <lucas_medeiros1@SNSOO1EP01VM013.saojose.sc.senai.br>
2015-11-06 19:52:38 -0200

    Entity vaga.

--- RHFacil/src/entity/Vaga.java
+++ RHFacil/src/entity/Vaga.java
@@ -1,16 +1,87 @@
 package entity;
 
 public class Vaga {
-    
-    private String temporaria;
+    private Integer idVagas;
+    private Integer temporaria;
     private String cargo;
     private String funcoesExercidas;
-    private String salario;
-    private String cargaHorariaSemanal;
-    private String disponibilidadeParaViagens;
-    private String deficiencia;
+    private Float salario;
+    private String cargaHoraria;
+    private Integer disponibilidadeViagem;
+    private Integer vagaPcd;
     private String cnh;
-    private String beneficios;
-    private String competencias;
+
+    public Integer getIdVagas() {
+        return idVagas;
+    }
+
+    public void setIdVagas(Integer idVagas) {
+        this.idVagas = idVagas;
+    }
+
+    public Integer getTemporaria() {
+        return temporaria;
+    }
+
+    public void setTemporaria(Integer temporaria) {
+        this.temporaria = temporaria;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getFuncoesExercidas() {
+        return funcoesExercidas;
+    }
+
+    public void setFuncoesExercidas(String funcoesExercidas) {
+        this.funcoesExercidas = funcoesExercidas;
+    }
+
+    public Float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Float salario) {
+        this.salario = salario;
+    }
+
+    public String getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(String cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public Integer getDisponibilidadeViagem() {
+        return disponibilidadeViagem;
+    }
+
+    public void setDisponibilidadeViagem(Integer disponibilidadeViagem) {
+        this.disponibilidadeViagem = disponibilidadeViagem;
+    }
+
+    public Integer getVagaPcd() {
+        return vagaPcd;
+    }
+
+    public void setVagaPcd(Integer vagaPcd) {
+        this.vagaPcd = vagaPcd;
+    }
+
+    public String getCnh() {
+        return cnh;
+    }
+
+    public void setCnh(String cnh) {
+        this.cnh = cnh;
+    }
+    
     
 }
